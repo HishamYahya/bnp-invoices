b134e7a910b33111eeed5de86433a9aeffb94779
Hisham Alyahya <Hishamaalyahya@gmail.com>
2019-07-04 22:30:08 +0300

    Routing v1

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import MainPage from './MainPage';
+import AppBar from './AppBar';
 
-export default () => {
-  return <div>App</div>;
+const PrivateRoute = ({
+  component: Component,
+  condition,
+  redirectTo = '/login',
+  ...rest
+}) => {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        return condition ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={redirectTo} />
+        );
+      }}
+    />
+  );
 };
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <AppBar />
+        <Switch>
+          <PrivateRoute path="/login" component={LoginPage} redirectTo="/" />
+          <PrivateRoute condition path="/" exact component={MainPage} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
